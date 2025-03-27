9c4525f98ae4724523d8c417ca2109ecf88f8cd3
Eduardo Castro <eduardojeshu@outlook.es>
2025-03-26 20:36:45 -0500

    EjercicioPrueba1

---
+++ PrograClase1_2025/PrograClase1_2025/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrograClase1_2025
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } }
+        public int Age { get { return age; } }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+    }
+}
